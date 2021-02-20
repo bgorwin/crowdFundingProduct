e0ab7c916e0a101ca455ec8680f9df09bfd80df7
Brent Gorwin <brent.gorwin@gmail.com>
2021-02-20 14:06:14 -0600

    Finished the navigation with the popup modal and background image.

---
+++ js/script.js
@@ -0,0 +1,19 @@
+// Navigation Modal
+function openNavigation() {
+	const navModal = document.getElementById('nav__menu--popup');
+
+	if (navModal.style.display === 'none') {
+		navModal.style.display = 'block';
+	} else {
+		navModal.style.display = 'none';
+	}
+}
+
+// function navHamburger(x) {
+// 	x.classList.toggle('change');
+// }
+
+// function closeNavigation() {
+// 	const navModal = document.getElementById('nav__menu--popup');
+// 	navModal.style.display = 'none';
+// }
