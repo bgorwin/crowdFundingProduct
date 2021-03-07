4ae107087a44f43d3494288e8abe81489c44dd37
Brent Gorwin <brent.gorwin@gmail.com>
2021-03-07 12:35:24 -0600

    finished all styling for mobile view. Finished modal popups for selecting project and thank you modal.

--- js/script.js
+++ js/script.js
@@ -10,12 +10,48 @@ function openNavigation() {
 }
 
 // Crowdfund Modal Start Bamboo Stand
-// function openModal() {
-// 	const startCrowdfundModal = document.getElementById('crowdfund__popup-start');
+function openBackThisProject() {
+	const startCrowdfundModal = document.getElementById(
+		'backThisProject__popup-modal'
+	);
 
-// 	if (startCrowdfundModal.style.display === 'none') {
-// 		startCrowdfundModal.stytle.display = 'block';
-// 	} else {
-// 		startCrowdfundModal.style.display = 'none';
-// 	}
-// }
+	if (startCrowdfundModal.style.display === 'none') {
+		startCrowdfundModal.style.display = 'block';
+	} else {
+		startCrowdfundModal.style.display = 'none';
+	}
+}
+
+function closeBackThisProject() {
+	const startCrowdfundModal = document.getElementById(
+		'backThisProject__popup-modal'
+	);
+
+	if (startCrowdfundModal.style.display === 'block') {
+		startCrowdfundModal.style.display = 'none';
+	} else {
+		startCrowdfundModal.style.display = 'none';
+	}
+}
+
+// Thank you modal popup
+function thankYouBanner() {
+	const thankModal = document.getElementById('thankYouModalPopup');
+
+	if (thankModal.style.display === 'none') {
+		thankModal.style.display = 'block';
+	} else {
+		thankModal.style.display = 'none';
+	}
+}
+
+// Close Thank You Modal
+function closeThankYouModal() {
+	const close = document.getElementById('thankYouModalPopup');
+
+	if (close.style.display === 'block') {
+		close.style.display = 'none';
+	} else {
+		close.style.display = 'none';
+	}
+}
